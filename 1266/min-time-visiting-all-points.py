d297b8d67d7a8ce449340339dad1c8b48155e333
Subhankar Biswas <subhankar@Subhankars-MacBook-Air.local>
2026-01-24 12:28:52 +0530

    min time visiting all points

---
+++ 1266/min-time-visiting-all-points.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+       sm = 0
+       for i in range(1,len(points)):
+       	sm += max(abs(points[i-1][0]-points[i][0]),abs(points[i-1][1]-points[i][1]))   
+       return sm
+
+
+
+if __name__ == "__main__":
+	s = Solution()
+	points = [[1,1],[3,4],[-1,0]]
+	print(s.minTimeToVisitAllPoints(points))
